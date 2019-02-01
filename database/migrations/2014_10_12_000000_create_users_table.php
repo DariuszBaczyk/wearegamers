a6110ea8d493feec5ec5bb6e1333dd0def25471c
szczesny <krzysztof.szczesny1994@gmail.com>
2019-02-01 13:02:34 +0100

    initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->nullable();            
+            $table->string('first_name')->nullable();
+            $table->string('surname')->nullable();
+            $table->text('sex')->nullable();
+            $table->date('birthday')->nullable();
+            $table->tinyInteger('role_id')->unsigned();
+            $table->string('about_me')->nullable();
+            $table->string('privacy_status')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('google_id')->nullable();
+            $table->string('avatar')->nullable();
+            $table->softDeletes();
+            $table->rememberToken()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Friend;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+    	//$faker = Faker::create('pl_PL');
+
+    	/* =============== VARIABLES =============== */
+
+    	/*$number_of_users = 20;
+    	$max_posts_per_user = 20;
+    	$max_comments_per_post = 5;
+    	$password = 'pass';*/
+
+    	DB::table('roles')->insert([
+    		'id' => 1,
+    		'type' => 'admin',
+    	]);
+
+    	DB::table('roles')->insert([
+    		'id' => 2,
+    		'type' => 'user',
+    	]);
+
+    	DB::table('roles')->insert([
+    		'id' => 3,
+    		'type' => 'owner',
+    	]);
+
+    	DB::table('roles')->insert([
+    		'id' => 4,
+    		'type' => 'editor',
+    	]);
+
+    	DB::table('users')->insert([
+		    		'name' => 'admin1',
+		    		'email' => 'admin1@wearegamers.co.pl',
+		    		'sex' => 'm',
+		    		'role_id' => 1,
+		    		'password' => bcrypt('zaq123'),
+		]);
+
+		DB::table('users')->insert([
+		    		'name' => 'szczesny1994',
+		    		'email' => 'krzysztof.szczesny1994@gmail.com',
+		    		'sex' => 'm',
+		    		'role_id' => 1,
+		    		'password' => bcrypt('zaq123'),
+		]);
+
+		DB::table('users')->insert([
+		    		'name' => 'diro231',
+		    		'email' => 'baczykdariusz@gmail.com',
+		    		'sex' => 'm',
+		    		'role_id' => 1,
+		    		'password' => bcrypt('zaq123'),
+		]);
+    }
+}
