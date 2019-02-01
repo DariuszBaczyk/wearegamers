8d2b2044e0e34282a06d260211a2ea4b9f55451a
szczesny <krzysztof.szczesny1994@gmail.com>
2019-02-01 13:12:52 +0100

    controllers added

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use App\User;
 
 class ImagesController extends Controller
 {
-    //
+	public function user_avatar($id, $size)
+	{
+		$user = User::findOrFail($id);
+
+		if (is_null($user->avatar)) {
+			if ($user->sex == 'm')
+			$img = Image::make('')->fit($size)->response('jpg', 90);
+			else
+			$img = Image::make('')->fit($size)->response('jpg', 90);	
+		} elseif (strpos($user->avatar, 'http') !== false) {
+			$img = Image::make($user->avatar)->fit($size)->response('jpg', 90);
+		} else {
+			$avatar_path = asset('storage/users/' . $id . '/avatars/' . $user->avatar);
+			$img = Image::make($avatar_path)->fit($size)->response('jpg', 90);
+		}
+
+		return $img;
+	}
 }
